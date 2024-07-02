f1d3038fd25d081c79152470cbc11109a9adcdd5
Cristian Loera <loera_73@hotmail.com>
2024-07-02 17:31:22 -0600

    Added the README file

--- models/index.js
+++ models/index.js
@@ -12,6 +12,7 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
@@ -22,7 +23,8 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'tag_id'
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
 })
 
 module.exports = {
